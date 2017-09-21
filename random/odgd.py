20ac353141ff4f87726c877ffb99c86f269916a2
Shree Ranga <shreerangaraju@gmail.com>
2017-09-21 02:50:34 -0400

    added maxiter and formated properly

--- random/odgd.py
+++ random/odgd.py
@@ -5,34 +5,40 @@ Wikipedia example
 """
 
 from __future__ import division
-import matplotlib.pyplot as plt
 import numpy as np
 
 # one dimension function x^4 - 3x^3 + 2
 def f_x(x):
     return x**4 - 3 * x**3 + 2
-x = np.arange(-10,11)
-y = []
-for i in x:
-    y.append(f_x(i))
 
-plt.plot(x,y)
-plt.show(   )
 
 # differentiation of the given function
 def df(x):
     return 4 * x**3 - 9 * x**2
 
-# GD hyperparameters
-gamma = 0.01 # step size multiplier
-init_x = 6 # randomly chosen x value for function/ x_0
-precision = 0.00001
-prev_step_size = init_x
+if __name__ == '__main__':
 
-while prev_step_size > precision:
-    last_x = init_x
-    init_x += -gamma * df(last_x)
-    prev_step_size = abs (init_x - last_x)
+    # Generate data
+    x = np.arange(-10,11)
+    y = []
+    for i in x:
+        y.append(f_x(i))
 
-print "The local minima occurs at {}".format(init_x)
+    # GD hyperparameters
+    gamma = 0.01 # step size multiplier
+    init_x = 6 # randomly chosen x value for function/ x_0
+    precision = 0.00001
+    prev_step_size = init_x
+    # max number of iterations maybe?
+    max_iter = 100
+    count = 0
+
+    while ((prev_step_size > precision) and (count < max_iter)):
+        print init_x
+        last_x = init_x
+        init_x += -gamma * df(last_x)
+        prev_step_size = abs (init_x - last_x)
+        count += 1
+
+    print "The local minima occurs at {}".format(init_x)
 
