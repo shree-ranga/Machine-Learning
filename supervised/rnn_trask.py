d3940485f23525efc3073049cf039c08ca6f073b
Shree Ranga <shreerangaraju@gmail.com>
2018-01-25 02:43:07 -0500

    understand better

--- supervised/rnn_trask.py
+++ supervised/rnn_trask.py
@@ -47,7 +47,7 @@ synapse_0_update = np.zeros_like(synapse_0)
 synapse_1_update = np.zeros_like(synapse_1)
 synapse_h_update = np.zeros_like(synapse_h)
 
-for j in xrange(2):
+for j in xrange(10000):
 
     # addition c = a + b
     a_int = np.random.randint(largest_number / 2)
@@ -72,4 +72,62 @@ for j in xrange(2):
     for position in xrange(binary_dim):
 
         # generate input and output
-        print position
+        X = np.array([[a[binary_dim - position - 1], b[binary_dim - position - 1]]])
+        y = np.array([c[binary_dim - position - 1]]).T
+
+        # hidden layer
+        layer_1 = sigmoid(np.dot(X, synapse_0) + np.dot(layer_1_values[-1], synapse_h))
+
+        # output layer
+        layer_2 = sigmoid(np.dot(layer_1, synapse_1))
+
+        layer_2_error = y - layer_2
+        layer_2_deltas.append(layer_2_error * sigmoid_output_to_derivative(layer_2))
+        overallError += np.abs(layer_2_error[0])
+
+        # decode estimate
+        d[binary_dim - position - 1] = np.round(layer_2[0][0])
+
+        # store hidden layer to use it in the next time step
+        layer_1_values.append(copy.deepcopy(layer_1))
+
+    future_layer_1_delta = np.zeros(hidden_dim)
+
+    for position in xrange(binary_dim):
+
+        X = np.array([[a[position], b[position]]])
+        layer_1 = layer_1_values[-position - 1]
+        prev_layer_1 = layer_1_values[-position - 2]
+
+        # error at output layer
+        layer_2_delta = layer_2_deltas[-position - 1]
+
+        # error at hidden layer
+        layer_1_delta = (future_layer_1_delta.dot(synapse_h.T) +
+                         layer_2_delta.dot(synapse_1.T)) * sigmoid_output_to_derivative(layer_1)
+
+        # update all weights
+        synapse_1_update += np.atleast_2d(layer_1).T.dot(layer_2_delta)
+        synapse_h_update += np.atleast_2d(prev_layer_1).T.dot(layer_1_delta)
+        synapse_0_update += X.T.dot(layer_1_delta)
+
+        future_layer_1_delta = layer_1_delta
+
+    synapse_0 += synapse_0_update * alpha
+    synapse_1 += synapse_1_update * alpha
+    synapse_h += synapse_h_update * alpha
+
+    synapse_0_update *= 0
+    synapse_h_update *= 0
+    synapse_1_update *= 0
+
+    # print progress
+    if (j % 1000 == 0):
+        print "Error" + str(overallError)
+        print "Pred" + str(d)
+        print "True" + str(c)
+        out = 0
+        for index, x in enumerate(reversed(d)):
+            out += x * pow(2, index)
+        print str(a_int) + " + " + str(b_int) + " = " + str(out)
+        print "-------------------------"
